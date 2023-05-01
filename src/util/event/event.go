ec81e27e5d4aa645f8a6235a8a64659eecad52d6
FillipdotS <fillip.serov@gmail.com>
2023-05-01 22:55:50 +0100

    Add NewRandom to event

--- src/util/event/event.go
+++ src/util/event/event.go
@@ -1,21 +1,34 @@
 package event
 
 import (
+	"math/rand"
+
 	"github.com/google/uuid"
 )
 
-type event struct {
+type Event struct {
 	Id    uuid.UUID
 	Buyer string
 	Item  string
 	Price int
 }
 
-func NewEvent(buyer string, item string, price int) event {
-	return event{
+func New(buyer string, item string, price int) Event {
+	return Event{
 		Id:    uuid.New(),
 		Buyer: buyer,
 		Item:  item,
 		Price: price,
 	}
+}
+
+func NewRandom() Event {
+	names := [...]string{"Fillip", "John", "Arthur", "Bob", "Gary", "Michael", "Mikhail", "Dmitry", "Sam", "Sally", "Jane", "Andrea", "Walter"}
+	items := [...]string{"Book", "Pen", "Pencil", "Notebook", "Laptop", "Phone", "Tablet", "Monitor", "Keyboard", "Mouse", "Headphones", "Speakers", "Microphone"}
+
+	return New(
+		names[rand.Intn(len(names))],
+		items[rand.Intn(len(items))],
+		rand.Intn(1000),
+	)
 }
